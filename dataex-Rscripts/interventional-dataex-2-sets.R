7bd329a2a4c655a669ac313a982719c701b89e1b
wwloh <wenwloh@gmail.com>
2020-06-14 16:28:55 +0200

    dataex CIs

--- dataex-Rscripts/interventional-dataex-2-sets.R
+++ dataex-Rscripts/interventional-dataex-2-sets.R
@@ -62,38 +62,14 @@ colnames(res_est.ci) <- c("est","l","u")
 rownames(res_est.ci) <- gene_names
 head(res_est.ci)
 
-# p-values: using increasingly larger CIs =====================================
-alphas <- (1:499)/500
-res_est.pv.alphas <- lapply(alphas, function(alpha) {
-  apply(apply(res_est, 1, bca, conf.level=1-alpha),2,function(ci) 
-    (ci[1] <= 0) & (ci[2] >= 0))
-})
-res_est.pv <- do.call(rbind,res_est.pv.alphas)
-res_est.pv <- apply(res_est.pv, 2, function(x) {
-  if(any(x==TRUE)) {
-    return(max(which(x==TRUE)))
-  } else {
-    return(1)
-  }
-})
-## replace with original names
-names(res_est.pv) <- gene_names
-
-# check CIs and p-values match up
-identical(which(res_est.pv<which(alphas==0.05)),
-          which((res_est.ci[,"l"] > 0) | (res_est.ci[,"u"] < 0)))
-res_est.ci <- cbind(res_est.ci,"pv"=alphas[res_est.pv])
-
 ## significant mediators in each gene set
 res_est.ci.signif <- NULL
 res_est.ci.signif.onetab <- NULL
 for (gs in huangpan_table2) {
   gs_idx <- which(gene_names %in% as.character(all.geneset[[gs]][,"x"]))
   res_est.ci.gs <- data.frame(res_est.ci[gs_idx,])
-  gs_signif <- which(res_est.ci.gs[,"pv"] < 0.05)
+  gs_signif <- which( (res_est.ci.gs[,"l"] > 0) | (res_est.ci.gs[,"u"] < 0) )
   res_est.ci.gs <- res_est.ci.gs[gs_signif,]
-  res_est.ci.gs <- res_est.ci.gs[order(res_est.ci.gs[,"pv"],decreasing=FALSE),]
-  res_est.ci.signif <- c(res_est.ci.signif, list(res_est.ci.gs))
   
   res_est.ci.gs.dt <- data.table(cbind(
     "gene"=row.names(res_est.ci.gs),res_est.ci.gs,"gs"=1))
@@ -107,12 +83,10 @@ for (gs in huangpan_table2) {
   }
   setkey(res_est.ci.signif.onetab)
 }
-names(res_est.ci.signif) <- huangpan_table2
 
 res_est.ci.signif.onetab[is.na(res_est.ci.signif.onetab)] <- 0
-print(xtable(res_est.ci.signif.onetab[
-  order(res_est.ci.signif.onetab$pv,-abs(res_est.ci.signif.onetab$est)),],
-  digits=c(0,0,rep(2,3),3,rep(0,9))),include.rownames=FALSE)
+print(xtable(res_est.ci.signif.onetab[order(res_est.ci.signif.onetab$l),],
+             digits=c(0,0,2,2,2,rep(0,9))),include.rownames=FALSE)
 
 # coefficients in outcome and mediator mean models ============================
 coef.ds <- do.call(rbind,lapply(1:length(res), function(s) 
@@ -128,21 +102,19 @@ for (gs in huangpan_table2) {
   # regression coefficients
   gs_regcoef <- coef.ds[gs_idx,]
   # indirect effects
-  gs_pv <- res_est.pv[gs_idx] # p-values
-  # check correct genes
-  if (!identical(row.names(gs_regcoef),names(gs_pv))) {
-    break()
-  }
-  gs_pv <- alphas[gs_pv]
+  res_est.ci.gs <- data.frame(res_est.ci[gs_idx,])
+  gs_ptsize <- pmin(exp(abs(res_est.ci.gs[,"est"]))*1.5,3)
+  gs_signif <- which( (res_est.ci.gs[,"l"] > 0) | (res_est.ci.gs[,"u"] < 0) )
+  
   plot(gs_regcoef,
        pch=20,
-       col=palette[cut(-log10(gs_pv), maxColorValue)],
-       cex=exp(1-gs_pv)*2/3,
+       col=palette[cut(gs_ptsize, maxColorValue)],
+       cex=gs_ptsize,
        xlim=range(coef.ds[,1])*1.1,ylim=range(coef.ds[,2]),
        xlab="M --> Y",ylab="A --> M",
        main=gs)
-  points(x=gs_regcoef[gs_pv<=0.05,"M1"],y=gs_regcoef[gs_pv<=0.05,"A"],
-         cex=exp(1-gs_pv[gs_pv<=0.05])*2/3)
+  points(x=gs_regcoef[gs_signif,"M1"],y=gs_regcoef[gs_signif,"A"],
+         cex=gs_ptsize)
   abline(h=0,col="grey50",lwd=.5)
   abline(v=0,col="grey50",lwd=.5)
 }
